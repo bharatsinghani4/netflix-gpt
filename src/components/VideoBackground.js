9951445ca2f193a3deeca20be637ee4c3485d0fc
Bharat Singhani <singhani.bharat7@gmail.com>
2025-04-16 06:08:32 +0530

    removed youtube suggestions at the end of the video

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -10,7 +10,7 @@ const VideoBackground = ({ movieId }) => {
     <div className="w-screen">
       <iframe
         className="w-screen aspect-video"
-        src={`https://www.youtube.com/embed/${trailer?.key}?&autoplay=1&loop=1&mute=1&showinfo=0&controls=0&modestbranding=1`}
+        src={`https://www.youtube.com/embed/${trailer?.key}?playlist=${trailer?.key}&autoplay=1&loop=1&mute=1&controls=0`}
         title="YouTube video player"
       ></iframe>
     </div>
